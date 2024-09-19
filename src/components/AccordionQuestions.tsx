64ad2710b4b666f484d68e9a58399d0379285d52
Alexandre Ferreira <breadmodz@gmail.com>
2024-09-19 00:59:14 -0300

    Update AccordionQuestions.tsx

--- src/components/AccordionQuestions.tsx
+++ src/components/AccordionQuestions.tsx
@@ -9,19 +9,19 @@ interface AccordionQuestionsProps {
 export function AccordionQuestions({ from, to }: AccordionQuestionsProps) {
   const data = [
     {
-      id: "1",
+      id: 1,
       title: "Posso fazer o pagamento por quais meios?",
       description:
         "Você pode pagar via PIX à vista por apenas R$ 97, ou se preferir, no crédito em 12x de R$ 9,74.",
     },
     {
-      id: "2",
+      id: 2,
       title: "Como vou receber o acesso ao método?",
       description:
         "Assim que o seu pagamento for confirmado você receberá um e-mail com seus dados de acesso na plataforma kiwify. Dependendo da forma de pagamento, é instantâneo.",
     },
     {
-      id: "3",
+      id: 3,
       title: "Como funciona as aulas?",
       description:
         "As aulas são gravadas e atualizadas constantemente. Você pode assistir tanto no celular quanto no computador.",
@@ -33,25 +33,25 @@ export function AccordionQuestions({ from, to }: AccordionQuestionsProps) {
         "Com o nosso método você vai aprender tudo o que é necessário para crescer e lucrar com o marketing digital apenas usando seu celular ou computador com acesso à internet.",
     },
     {
-      id: "5",
+      id: 5,
       title: "Em quanto tempo vou começar a ter resultados ?",
       description:
         "Tudo depende da sua disponibilidade para estudar e praticar o que ensinamos. Em média 3 semanas nossos alunos já estão colhendo frutos do seu trabalho.",
     },
     {
-      id: "6",
+      id: 6,
       title: "Por quanto tempo terei acesso ao curso?",
       description:
         "O acesso é vitalício, com direito a atualizações das aulas sem pagar nada a mais por isso.",
     },
     {
-      id: "7",
+      id: 7,
       title: "Existe alguma garantia?",
       description:
         "Sim, se você não gostar do conteúdo, poderá pedir seu dinheiro de volta, sem nenhum questionamento em até 7 dias.",
     },
     {
-      id: "8",
+      id: 8,
       title: "Tem suporte ao aluno?",
       description:
         "Você vai ter acesso ao nosso suporte exclusivo diretamente no WhastApp.",
@@ -64,7 +64,7 @@ export function AccordionQuestions({ from, to }: AccordionQuestionsProps) {
         return (
           <Accordion.Item
             className="overflow-hidden first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10"
-            value={question.id}
+            value={question.id.toString()}
             key={question.id}
           >
             <Accordion.Trigger className="text-violet11 shadow-mauve6 transition-all hover:bg-mauve2 flex py-6 flex-1 cursor-pointer items-center justify-between bg-white px-5 text-[15px] leading-none shadow-[0_1px_0] w-full group">
