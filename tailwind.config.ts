102e417749232dcd72d62942853d71ed95111ce5
Alexandre Ferreira <breadmodz@gmail.com>
2024-09-19 01:01:13 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,12 +21,12 @@ const config: Config = {
       },
       keyframes: {
         slideDown: {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         slideUp: {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
